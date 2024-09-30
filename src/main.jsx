4d04ae3677ea909945933764b0ac7a9a3b77cdbc
TonyChopper64 <poojamayekar.9526@gmail.com>
2024-09-30 16:32:43 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App.jsx";
-import "./css/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
