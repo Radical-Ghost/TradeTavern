084e4c92ced8956089f73534627057897ec5a0ad
Soham Mandavkar <radicalghost98@gmail.com>
2024-09-30 16:54:58 +0530

    resolved some conflicts

--- src/components/Invest.jsx
+++ src/components/Invest.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import Navigationbar from './comp/Navbar';
-import Graph from './comp/StockGraph';
+import React from "react";
+import Navigationbar from "./comp/Navbar";
+import Graph from "./comp/StockGraph";
 
 const Invest = () => {
-  return (
-    <div style={{ display: 'flex' }}>
-      <Navigationbar/>
-      <div className="content p-4" style={{marginTop:'-23px' , marginLeft:'-24px' , width: '100%', padding:'0px' , width: '100%'}}>
-        <h1>Invest page</h1>
-        
-      </div>
-    </div>
-  );
+	return (
+		<div style={{ display: "flex" }}>
+			<Navigationbar />
+			<div
+				className="content p-4"
+				style={{
+					marginTop: "-23px",
+					marginLeft: "-24px",
+					width: "100%",
+					padding: "0px",
+				}}>
+				<h1>Invest page</h1>
+			</div>
+		</div>
+	);
 };
 
 export default Invest;
