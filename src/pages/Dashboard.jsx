8faf88542af8a4c678d9cac1e1ca642cc0cb53e3
Soham Mandavkar <Soham Mandavkar>
2025-02-11 12:38:08 +0530

    .

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,9 @@ export default function Dashboard() {
 			<div style={{ display: "flex", width: "100%" }}>
 				<div className="content" style={{ width: "100%" }}>
 					{/* Dashboard content */}
+
 					<h1>Dashboard</h1>
+					<h2>soham is a gay </h2>
 				</div>
 			</div>
 		</>
